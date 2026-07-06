9934f2ab267638d70022ab909cc730bdb38dd733
RealMir5713 <walkerghsky95@gmail.com>
2026-07-06 21:25:41 +0700

    Update site.ts

--- data/site.ts
+++ data/site.ts
@@ -153,7 +153,7 @@ export const sponsorTiers: Array<{ tier: string; logos: SponsorLogo[] }> = [
     logos: [
       { name: "MSB", src: "/images/msb-logo.png", href: "https://www.msb.com.vn/" },
       { name: "Edu2Review", src: "/images/sponsors/edu2review_new.png", href: "https://edu2review.com/" },
-      { name: "Eduwing Global", src: "/images/sponsors/edu2review_2.png", href: "https://eduwing.vn/" },
+      { name: "Eduwing Global", src: "/images/sponsors/edu2review_2.png", href: "https://eduwingglobal.com/" },
       { name: "Advertising Vietnam", src: "/images/sponsors/ad_vietnam.png", href: "https://advertisingvietnam.com" }
     ]
   },
